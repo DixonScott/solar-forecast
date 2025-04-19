2672aea55ec713abb7c96422082a1fccead97dea
DixonScott <scottdixon195@gmail.com>
2025-04-19 14:05:13 +0100

    Function "get_weather_for_locations" breaks down the input into batches of locations that obey the API limit.

--- openmeteo.py
+++ openmeteo.py
@@ -2,9 +2,8 @@ import time
 import pandas as pd
 
 import openmeteo_requests
-from requests.adapters import HTTPAdapter
-from urllib3.util.retry import Retry
-import requests
+import requests_cache
+from retry_requests import retry
 
 
 OPEN_METEO_START_DATE = pd.Timestamp("2022-03-01")
@@ -38,7 +37,7 @@ def api_cost_calc(table_of_pv_systems, date_format = "%Y-%m-%d"):
     return result
 
 
-def get_weather_for_locations(query, daily_vars = DAILY_VARS, date_format = "%Y-%m-%d", output_file_name = "weather.csv"):
+def get_weather_for_locations(query, daily_vars = DAILY_VARS, date_format = "%Y-%m-%d"):
     if isinstance(query, str):
         print(f"Assuming that {query} is the name of a file...")
         with open("data/" + query, "r") as file:
@@ -50,65 +49,66 @@ def get_weather_for_locations(query, daily_vars = DAILY_VARS, date_format = "%Y-
 
     query.loc[query["Earliest Output Date"] < OPEN_METEO_START_DATE, "Earliest Output Date"] = OPEN_METEO_START_DATE
 
-    retry_strategy = Retry(
-        total = 3,
-        backoff_factor = 1,
-        status_forcelist = [502, 503, 504],
-        allowed_methods = ["GET"]
-    )
-    adapter = HTTPAdapter(max_retries=retry_strategy)
-    session = requests.Session()
-    session.mount("http://", adapter)
-    session.mount("https://", adapter)
-    open_meteo = openmeteo_requests.Client(session=session)
+    api_cost = api_cost_calc(query)
+    if api_cost.sum() > MAX_API_CALLS_HOUR:
+        print("Total API cost exceeds the hourly limit of 5000.")
+        return
+    if api_cost.max() > MAX_API_CALLS_MINUTE:
+        print("API cost of at least one location exceeds the minutely limit of 600.")
+        return
+
+    total = 0
+    start_of_chunk = 0
+    chunks = []
+
+    for i, n in enumerate(api_cost):
+        total += n
+        if total > MAX_API_CALLS_MINUTE:
+            chunks.append(query.iloc[start_of_chunk:i])
+            start_of_chunk = i
+            total = n
+    chunks.append(query.iloc[start_of_chunk:])
+
+    # Set up the Open-Meteo API client with cache and retry on error
+    cache_session = requests_cache.CachedSession('.cache', expire_after = 3600)
+    retry_session = retry(cache_session, retries = 5, backoff_factor = 0.2)
+    open_meteo = openmeteo_requests.Client(session = retry_session)
 
     location_dfs = []
-    failed_locations = []
     if "System ID" in query:
         location_ids = (i for i in query["System ID"])
     else:
         location_ids = (i for i in range(len(query)))
-    first_idx = query.index[0]
-    for idx, location in query.iterrows():
-        location_id = next(location_ids)
-        if idx != first_idx:
-            print("Waiting 10s before next API call...")
-            time.sleep(10)
+    for i, chunk in enumerate(chunks):
+        if i > 0:
+            print("Waiting 60s before next API call...")
+            time.sleep(60)
 
         params = {
-            "latitude": location["Latitude"],
-            "longitude": location["Longitude"],
-            "start_date": location["Earliest Output Date"].strftime("%Y-%m-%d"),
-            "end_date": location["Latest Output Date"].strftime("%Y-%m-%d"),
+            "latitude": chunk["Latitude"].tolist(),
+            "longitude": chunk["Longitude"].tolist(),
+            "start_date": chunk["Earliest Output Date"].dt.strftime("%Y-%m-%d").tolist(),
+            "end_date": chunk["Latest Output Date"].dt.strftime("%Y-%m-%d").tolist(),
             "daily": list(daily_vars)
         }
-        print(f"{time.strftime("%H:%M:%S", time.localtime())} - Making request for id {location_id}...")
-        try:
-            responses = open_meteo.weather_api(OPEN_METEO_URL, params = params)
-        except Exception as e:
-            print(f"{time.strftime("%H:%M:%S", time.localtime())} - Request for id {location_id} failed.")
-            print(f"An error occurred: {e}")
-            continue
-        print(f"{time.strftime("%H:%M:%S", time.localtime())} - Request {location_id} successful.")
+        responses = open_meteo.weather_api(OPEN_METEO_URL, params = params)
 
-        response = responses[0]
-        daily = response.Daily()
-        daily_data = {
-            "id": location_id,
-            "date": pd.date_range(
-                start = pd.to_datetime(daily.Time(), unit = "s"),
-                end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
-                freq = pd.Timedelta(seconds = daily.Interval()),
-                inclusive = "left"
-            )
-        }
-        for var_no, var_name in enumerate(daily_vars):
-            daily_data[var_name] = daily.Variables(var_no).ValuesAsNumpy()
+        for response in responses:
+            daily = response.Daily()
+            daily_data = {
+                "id": next(location_ids),
+                "date": pd.date_range(
+                    start = pd.to_datetime(daily.Time(), unit = "s"),
+                    end = pd.to_datetime(daily.TimeEnd(), unit = "s"),
+                    freq = pd.Timedelta(seconds = daily.Interval()),
+                    inclusive = "left"
+                )
+            }
+            for var_no, var_name in enumerate(daily_vars):
+                daily_data[var_name] = daily.Variables(var_no).ValuesAsNumpy()
 
-        location_dfs.append(pd.DataFrame(data = daily_data))
+            location_dfs.append(pd.DataFrame(data = daily_data))
 
-    if location_dfs:
-        open_meteo_df = pd.concat(location_dfs, ignore_index = True)
-        open_meteo_df.to_csv("data/" + output_file_name, index = False)
-        return open_meteo_df
-    return
+    open_meteo_df = pd.concat(location_dfs, ignore_index = True)
+    open_meteo_df.to_csv("data/Weather_v1_pt1.csv", index = False)
+    return open_meteo_df
