093baef5c3921b3219b6cc85554a475dd791e6a8
DixonScott <scottdixon195@gmail.com>
2025-04-23 12:53:57 +0100

    Added function to combine the weather dataframe and solar panel output dataframe into one.

--- data_handling.py
+++ data_handling.py
@@ -17,3 +17,26 @@ def standardize_input(input_df_or_path, date_format):
     else:
         raise ValueError("Input must be a pandas DataFrame or a valid path to a CSV file.")
     return df
+
+
+def combine_weather_and_pvoutput(weather_df, pvoutput_df, filename):
+    if isinstance("data/" + weather_df, str):
+        weather_df = pd.read_csv("data/" + weather_df, parse_dates=["date"])
+    if isinstance(pvoutput_df, str):
+        with open("data/" + pvoutput_df, "r") as file:
+            split_idx = next((idx for idx, line in enumerate(file) if line.strip("\n,") == ""), 0)
+        pvoutput_df = pd.read_csv(
+            "data/" + pvoutput_df,
+            parse_dates=["Date"],
+            header=split_idx
+        )
+
+    dataset = pd.merge(pvoutput_df, weather_df, left_on=["System ID", "Date"], right_on=["id", "date"], how="inner")
+    dataset.drop(columns=['Energy Generated (Wh)',
+       'Energy Exported (Wh)', 'Energy Used (Wh)', 'Peak Power (W)',
+       'Peak Time', 'Condition', 'Min Temp (°C)', 'Max Temp (°C)',
+       'Peak Energy Import (Wh)', 'Off Peak Energy Import (Wh)',
+       'Shoulder Energy Import (Wh)', 'High Shoulder Energy Import (Wh)',
+       'Insolation (Wh)', 'id', 'date'], inplace = True)
+    dataset.to_csv("data/" + filename, index=False)
+    return dataset
--- main.py
+++ main.py
@@ -1,23 +1,15 @@
 import openmeteo
 import pvoutput
+import data_handling
 
-
-PVOUTPUT_ENABLED = False
-OPEN_METEO_ENABLED = True
 
 SYSTEM_IDS = [
     66991
 ]
-QUERY = "test.csv"
 
 
 def main():
-    if PVOUTPUT_ENABLED:
-        pvoutput.save_outputs_to_csv(SYSTEM_IDS, filename = "test.csv")
-
-    if OPEN_METEO_ENABLED:
-        openmeteo.get_weather_for_locations(QUERY, date_format = "%d/%m/%Y")
-
+    return
 
 if __name__ == '__main__':
     main()
