552fc8f105242b14bf525c36efb0a326f1d817ae
DixonScott <scottdixon195@gmail.com>
2025-07-10 18:01:00 +0100

    Jupyter notebook documenting data cleaning process and reasoning. Renamed data_exploration.py to data_cleaning.py.

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import data_exploration
+import data_cleaning
 import openmeteo
 import pvoutput
 import data_handling
@@ -13,7 +13,7 @@ def main():
     system_df, pvoutput_df = pvoutput.save_outputs_to_csv(SYSTEM_IDS, mode="full", filename="pvoutput.csv")
     weather_df = openmeteo.get_weather_for_locations(system_df, output_file_name="weather.csv")
     dataset = data_handling.combine_weather_and_pvoutput(weather_df, pvoutput_df, "dataset.csv")
-    dataset = data_exploration.clean_dataset(dataset)
+    dataset = data_cleaning.clean_dataset(dataset)
 
 if __name__ == '__main__':
     main()
